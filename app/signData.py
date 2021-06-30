b2436c53074640eefe1e31ffc76185d0c2443a3c
Felix <fdanielestevez98@gmail.com>
2021-06-30 19:24:03 -0400

    add sign api

---
+++ app/signData.py
@@ -0,0 +1,23 @@
+from config import client
+from app import app
+from flask import jsonify
+
+# Select the database
+db = client.restfulapi
+
+# Select the collection
+collection = db.signs
+
+@app.route("/")
+def get_initial_response():
+    """Welcome message for the API."""
+    # Message to the user
+    message = {
+        'apiVersion': 'v1.0',
+        'status': '200',
+        'message': 'Welcome to the Flask API'
+    }
+    # Making the message looks good
+    resp = jsonify(message)
+    # Returning the object
+    return resp
