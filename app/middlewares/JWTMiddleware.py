1a62c9ec0e7e85980d53da0b4ee60763d500618d
Felix <fdanielestevez98@gmail.com>
2021-07-21 17:23:02 -0400

    token validation

--- app/middlewares/JWTMiddleware.py
+++ app/middlewares/JWTMiddleware.py
@@ -1,11 +1,10 @@
 from werkzeug.wrappers import Request, Response
 from dotenv import dotenv_values
-from flask import jsonify
 
 from app.utils.JWToken import verify_token
 
 
-class JWTMiddleware():
+class JWTMiddleware:
     '''
     Simple WSGI middlewares
     '''
@@ -22,8 +21,11 @@ class JWTMiddleware():
             res = Response('Token Missing', mimetype='application/json', status=401)
             return res(environ, start_response)
 
+        secret = self.config['JWT_SECRET_TOKEN']
+        token = auth_header.replace('Bearer ', '')
+
         # verify the token
-        if verify_token(auth_header, self.config['JWT_SECRET_TOKEN']):
+        if verify_token(token, secret):
             return self.app(environ, start_response)
 
         res = Response(u'Authorization failed', mimetype='text/plain', status=401)
